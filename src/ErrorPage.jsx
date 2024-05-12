f60554f371d38e0a972067d1dc4e5f7272c62394
firoz.nur <firoznur5@gmail.com>
2024-05-12 17:38:25 +0600

    errorPage done

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import errorAnimation from "../src/assets/Animation404.json";
+import Lottie from "lottie-react";
+
+const ErrorPage = () => {
+  return (
+    <div
+      id="error-page"
+      className="flex h-screen flex-col items-center justify-center text-center"
+    >
+      <Lottie
+        animationData={errorAnimation}
+        loop={true}
+        autoplay={true}
+        style={{ width: "400", height: "400" }}
+      />
+
+      <div className="text-2xl font-bold md:text-4xl">
+        Something&apos;s Wrong Here... <br />
+        <p className="mt-10 text-base font-normal">
+          We cann&apos;t find the page you are looking for...
+        </p>
+        <br />
+        <Link className="btn btn-outline text-lg " to="/">
+          Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "../Root";
+import ErrorPage from "./ErrorPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
